8bf65f1a630679c5ec7f53de27f5ea02593bb444
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-09-01 00:12:12 +0530

    Updates

---
+++ precache-manifest.9f6948c5b40331668715ca03148244df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2977a53dd320f92058077c03ffcf3f9",
+    "url": "/kommunicate-task/index.html"
+  },
+  {
+    "revision": "e90c47af41da0a4fb792",
+    "url": "/kommunicate-task/static/css/main.c264e2f4.chunk.css"
+  },
+  {
+    "revision": "b8fa95ef4c08511b4523",
+    "url": "/kommunicate-task/static/js/2.b8b5bbd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kommunicate-task/static/js/2.b8b5bbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90c47af41da0a4fb792",
+    "url": "/kommunicate-task/static/js/main.7b39b78b.chunk.js"
+  },
+  {
+    "revision": "9673bbc9ebf6256ab431",
+    "url": "/kommunicate-task/static/js/runtime-main.bd897d78.js"
+  },
+  {
+    "revision": "899d24264a7ba9816efcbfec06802ec1",
+    "url": "/kommunicate-task/static/media/background.899d2426.png"
+  },
+  {
+    "revision": "540cc743d9756a5b5ce3706249766163",
+    "url": "/kommunicate-task/static/media/bg.540cc743.png"
+  }
+]);
