e39dd28497c32d9abfb715174902988a2ef9ef01
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-08-31 23:48:50 +0530

    added prev and next for pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 
 const Div = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
+  @media (max-width: 768px) {
+    margin: 0.5rem 0;
+  }
 `;
 
 const Button = styled.button`
   background: white;
   padding: 0.5rem 1rem;
+  outline: none;
   margin: 0 0.5rem;
   border: 1px solid #d9d9d9;
   transition: border 250ms ease-in;
@@ -18,13 +22,28 @@ const Button = styled.button`
   }
 `;
 
-const Pagination: React.FC<{ paginationHandler: (pageNo: number) => void }> = ({
-  paginationHandler,
-}) => (
+interface Pagination {
+  pageNo: number;
+  paginationHandler: (event: number) => void;
+}
+
+const Pagination: React.FC<Pagination> = ({ pageNo, paginationHandler }) => (
   <Div>
+    <Button
+      onClick={paginationHandler.bind(null, pageNo - 1)}
+      disabled={pageNo < 2}
+    >
+      Prev
+    </Button>
     <Button onClick={paginationHandler.bind(null, 1)}>1</Button>
     <Button onClick={paginationHandler.bind(null, 2)}>2</Button>
     <Button onClick={paginationHandler.bind(null, 3)}>3</Button>
+    <Button
+      onClick={paginationHandler.bind(null, pageNo + 1)}
+      disabled={pageNo > 2}
+    >
+      Next
+    </Button>
   </Div>
 );
 
